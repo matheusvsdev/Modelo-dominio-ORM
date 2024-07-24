fa03b59d7516bb0d267b7ab5c6445c07863a0360
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2024-07-24 15:51:40 -0300

    Segundo commit

--- src/main/java/com/matheusdev/modelo_dominio_orm/entities/Participante.java
+++ src/main/java/com/matheusdev/modelo_dominio_orm/entities/Participante.java
@@ -1,2 +1,63 @@
-package com.matheusdev.modelo_dominio_orm.entities;public class Participante {
+package com.matheusdev.modelo_dominio_orm.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "participante")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    @ManyToMany
+    @JoinTable(name = "tb_participante_atividade",
+            joinColumns = @JoinColumn(name = "participante_id"),
+            inverseJoinColumns = @JoinColumn(name = "atividade_id"))
+    private Set<Atividade> atividades = new HashSet<>();
+
+    public Participante() {
+    }
+
+    public Participante(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
 }
